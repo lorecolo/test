a560e0885e1ca897d990d476d27250d178b64b23
lorecolo <lorenzo.colonniello@insiel.it>
2015-02-06 15:33:49 +0100

    test 1

--- TestGit/src/test/git/GitTest.java
+++ TestGit/src/test/git/GitTest.java
@@ -3,7 +3,7 @@ package test.git;
 public class GitTest {
 
 	 public static void main(String[] args) {
-		    System.out.println("Git is fun");
+		    System.out.println("Git is funwwwww");
 		  }
 		
 	
